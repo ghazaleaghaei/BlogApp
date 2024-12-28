5a643b1f9a203826baf8144bb22f29f67f744052
ghazaleaghaei <ghazale.ag@gmail.com>
2024-12-28 15:58:21 +0330

    commited by script

--- src/app/(dashboard)/profile/posts/page.jsx
+++ src/app/(dashboard)/profile/posts/page.jsx
@@ -1,12 +1,27 @@
 import { Suspense } from "react"
 import PostsTable from "./_/components/PostsTable"
 import Fallback from "../_components/Fallback"
+import Search from "app/(posts)/posts/_/components/Search"
+import { CreatePost } from "./_/components/Buttons"
+import queryString from "query-string"
 
-function Posts() {
+async function Posts({ searchParams }) {
+
+    const query = queryString.stringify(await searchParams);
+
     return (
-        <Suspense fallback={< Fallback />}>
-            <PostsTable />
-        </Suspense>
+        <div>
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 text-secondary-700 mb-12 items-center">
+                <h1 className="font-bold text-xl">
+                    لیست پست ها
+                </h1>
+                <Search />
+                <CreatePost />
+            </div>
+            <Suspense fallback={< Fallback />} key={query}>
+                <PostsTable query={query} />
+            </Suspense>
+        </div>
     )
 }
 export default Posts
